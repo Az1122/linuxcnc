de01ee406412ba3bf530cd55c0d0bce5a0d4535a
Dewey Garrett <dgarrett@panix.com>
2017-01-04 20:06:23 -0700

    gladevcp: support auxiliary applications
    1) For auxiliary applications installed from a deb that
       is located in a known toplevel directory defined
       by the substitution variable @LINUXCNC_AUX_GLADEVCP@
       The location can be obtained using the linuxcnc_var
       script which should always be in PATH for either a
       RIP build or a deb install of LinuxCNC.  Typically:
       @LINUXNC_AUX_GLADEVCP@ = /usr/share/linuxcnc_aux_gladevcp
    
    2) For testing development of auxiliary applications, use
       an exported environmental variable (GLADEVCP_EXTRAS)
       to specify a colon (:) separated list of app directories
    
       Example:
       $ export GLADEVCP_EXTRAS=/home/test1:/home/test2
    
       Typically one would include the export statement in a startup
       script like ~/.profile or ~/.bashrc
    
    Notes:
      a) an exclude list is used to exlude problematic filenames
         that may be used for other purposes
         (like *setup*.py, *install*.py)
      b) app filenames that are duplicated are rejected
    
    Signed-off-by: Dewey Garrett <dgarrett@panix.com>

--- lib/python/gladevcp/__init__.py
+++ lib/python/gladevcp/__init__.py
@@ -1,1 +1,59 @@
 from hal_pythonplugin import *
+
+#----------------------------------------------------------------------
+# Support auxiliry gladevcp apps
+import os,sys,glob,importlib,subprocess
+
+def excluded_filename(fname):
+    exclude_list = ["install","setup"]
+    for e in exclude_list:
+        if e in fname: return True
+    return False
+
+#----------------------------------------------------------------------
+# Auxiliary gladevcp apps may be installed in a known location
+# location defined by the substitution item LINUXCNC_AUX_GLADEVCP.
+# The location is available from the script linuxcnc_var
+# (this script should always be in PATH for both RIP builds and
+# deb installs of LinuxCNC)
+s = subprocess.Popen(['linuxcnc_var','LINUXCNC_AUX_GLADEVCP']
+                    ,stdout=subprocess.PIPE
+                    ,stderr=subprocess.PIPE
+                    )
+p,e = s.communicate()
+gladevcp_aux_apps_dir = p.strip() # remove trailing \n
+
+modnames = []
+for auxdir in glob.glob(gladevcp_aux_apps_dir + "/*"):
+    print "gladevcp auxdir=",auxdir
+    for fname in glob.glob(auxdir + "/*.py"):
+        if excluded_filename(fname):
+            print "gladevcp: excluded:",fname
+            continue
+        modname = os.path.basename(fname).split(".")[0]
+        if modname in modnames:
+            print "gladevcp: rejecting duplicated name:",fname
+            continue
+        modnames.append(modname)
+        sys.path.insert(0,auxdir) # prepend
+        importlib.import_module(modname)
+        print "gladevcp: importing:",modname
+
+#----------------------------------------------------------------------
+# Auxiliary gladevcp apps specified by environmental variable
+gladevcp_user_extras = os.getenv('GLADEVCP_EXTRAS')
+if gladevcp_user_extras is not None:
+    print "gladevcp: GLADEVCP_EXTRAS:",gladevcp_user_extras
+    for extradir in gladevcp_user_extras.split(":"):
+        for fname in glob.glob(extradir + "/*.py"):
+            if excluded_filename(fname):
+                print "gladevcp: excluded:",fname
+                continue
+            modname = os.path.basename(fname).split(".")[0]
+            if modname in modnames:
+                print "gladevcp: rejecting duplicated name:",fname
+                continue
+            modnames.append(modname)
+            sys.path.insert(0,extradir) # prepend
+            importlib.import_module(modname)
+            print "gladevcp: importing:",modname
